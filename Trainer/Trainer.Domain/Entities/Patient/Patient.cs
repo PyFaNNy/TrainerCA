1c81580cdddce4465283f68cec05dc69590368a2
Artyom Puzyrev <fanny.koob@gmail.com>
2022-02-20 21:53:15 +0300

    init domain

---
+++ Trainer/Trainer.Domain/Entities/Patient/Patient.cs
@@ -0,0 +1,72 @@
+﻿using Trainer.Enums;
+
+namespace Trainer.Domain.Entities.Patient
+{
+    public class Patient
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string MiddleName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public Sex Sex
+        {
+            get;
+            set;
+        }
+
+        public IList<Examination.Examination> Examinations
+        {
+            get;
+            set;
+        }
+
+        public IList<Result.Result> Results
+        {
+            get;
+            set;
+        }
+
+        public string About
+        {
+            get;
+            set;
+        }
+
+        public string Hobbies
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Trainer/Trainer.Domain/Entities/Result/Result.cs
@@ -0,0 +1,63 @@
+﻿namespace Trainer.Domain.Entities.Result
+{
+    public class Result
+    {
+        public Guid Id
+        {
+            get;
+            set;
+        }
+
+        public Guid? PatientId
+        {
+            get;
+            set;
+        }
+
+        public Guid? ExaminationId
+        {
+            get;
+            set;
+        }
+
+        public int AverageHeartRate
+        {
+            get;
+            set;
+        }
+        public int AverageDia
+        {
+            get;
+            set;
+        }
+
+        public int AverageSis
+        {
+            get;
+            set;
+        }
+
+        public int AverageOxigen
+        {
+            get;
+            set;
+        }
+        public double AverageTemperature
+        {
+            get;
+            set;
+        }
+
+        public Patient.Patient Patient
+        {
+            get;
+            set;
+        }
+
+        public Examination.Examination Examination
+        {
+            get;
+            set;
+        }
+    }
+}
