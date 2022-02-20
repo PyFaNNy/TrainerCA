f140fa2a017f3e6ff355bd876ed60a41e8412188
Artyom Puzyrev <fanny.koob@gmail.com>
2022-02-20 21:52:56 +0300

    add old controllers

---
+++ Trainer/Trainer/Controllers/AccountController.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trainer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ITrainerDbContextService _contextService;
+        private readonly IMapper _mapper;
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(IContextService serv, IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _contextService = serv ?? throw new ArgumentNullException($"{nameof(serv)} is null.");
+            _mapper = mapper ?? throw new ArgumentNullException($"{nameof(mapper)} is null.");
+            _userManager = userManager ?? throw new ArgumentNullException($"{nameof(userManager)} is null.");
+            _signInManager = signInManager ?? throw new ArgumentNullException($"{nameof(signInManager)} is null.");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    MiddleName = model.MiddleName,
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    Status = StatusUser.Active
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                await _userManager.AddToRoleAsync(user, "Unknown");
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+                    if (result.Succeeded)
+                    {
+                       var res = await _userManager.UpdateAsync(user);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Incorrect login and (or) password");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> GetModels(SortState sortOrder = SortState.FirstNameSort)
+        {
+            ViewData["EmailSort"] = sortOrder == SortState.EmailSort ? SortState.EmailSortDesc : SortState.EmailSort;
+            ViewData["FirstNameSort"] = sortOrder == SortState.FirstNameSort ? SortState.FirstNameSortDesc : SortState.FirstNameSort;
+            ViewData["LastNameSort"] = sortOrder == SortState.LastNameSort ? SortState.LastNameSortDesc : SortState.LastNameSort;
+            ViewData["MiddleNameSort"] = sortOrder == SortState.MiddleNameSort ? SortState.MiddleNameSortDesc : SortState.MiddleNameSort;
+
+            var users = await _contextService.GetUsers(sortOrder);
+            return View(users);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Block(string[] selectedUsers)
+        {
+            foreach (var str in selectedUsers)
+            {
+                User user = await _userManager.FindByNameAsync(str);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                user.Status = StatusUser.Block;
+                await _userManager.UpdateAsync(user);
+                if (user.Status.Equals("blocked") && User.Identity.Name.Equals(user.UserName))
+                {
+                    await _signInManager.SignOutAsync();
+                }
+            }
+
+            return RedirectToAction("AdminPanel");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> UnBlock(string[] selectedUsers)
+        {
+            foreach (var str in selectedUsers)
+            {
+                User user = await _userManager.FindByNameAsync(str);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                user.Status = StatusUser.Active;
+                await _userManager.UpdateAsync(user);
+            }
+            return RedirectToAction("AdminPanel");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Delete(string[] selectedUsers)
+        {
+            foreach (var str in selectedUsers)
+            {
+                User user = await _userManager.FindByNameAsync(str);
+                if (user != null)
+                {
+                    if (User.Identity.Name.Equals(user.UserName))
+                    {
+                        await _signInManager.SignOutAsync();
+                    }
+                    IdentityResult result = await _userManager.DeleteAsync(user);
+                }
+            }
+            if (_userManager.FindByNameAsync(User.Identity.Name).Status.Equals("blocked"))
+            {
+                return Redirect("~/Account/Logout");
+            }
+            return RedirectToAction("AdminPanel");
+        }
+    }
+}
---
+++ Trainer/Trainer/Controllers/RolesController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trainer.DAL.Entities;
+using Trainer.Models;
+
+namespace Trainer.Controllers
+{
+    [Route("[controller]/[action]")]
+    [Authorize(Roles = "Admin")]
+    public class RolesController : Controller
+    {
+        RoleManager<Role> _roleManager;
+        UserManager<User> _userManager;
+        public RolesController(RoleManager<Role> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new Role(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UserList() => View(_userManager.Users.ToList());
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string userId)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = userId,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                var addedRoles = roles.Except(userRoles);
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("AdminPanel", "Account");
+            }
+
+            return NotFound();
+        }
+    }
+}
