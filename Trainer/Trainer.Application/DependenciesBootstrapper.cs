990c015c43355f5dd18144df40fd6af81c5d5dda
Artyom Puzyrev <fanny.koob@gmail.com>
2022-02-20 21:53:27 +0300

    init application

---
+++ Trainer/Trainer.Application/DependenciesBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿namespace Trainer.Application
+{
+    using System.Reflection;
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+    using Trainer.Application.Behaviours;
+
+    public static class DependenciesBootstrapper
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            return services;
+        }
+    }
+}
---
+++ Trainer/Trainer.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,53 @@
+﻿namespace Trainer.Application.Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentValidation.Results;
+
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            : base("One or more validation failures have occurred.")
+        {
+            this.Errors = new Dictionary<string, string[]>();
+            this.ErrorCodes = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+            : this()
+        {
+            this.Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+
+            this.ErrorCodes = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorCode)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+
+        }
+
+        public IDictionary<string, string[]> Errors
+        {
+            get;
+        }
+
+        public IDictionary<string, string[]> ErrorCodes
+        {
+            get;
+        }
+
+        public ValidationException(string propertyName, string errorMessage)
+           : this()
+        {
+            this.Errors.Add(propertyName, new string[] { errorMessage });
+            this.ErrorCodes.Add(propertyName, new string[] { propertyName });
+        }
+
+        public ValidationException(string propertyName, string errorCode, string errorMessage) : this()
+        {
+            this.ErrorCodes.Add(propertyName, new [] {errorCode});
+            this.Errors.Add(propertyName, new [] { errorMessage });
+        }
+    }
+}
