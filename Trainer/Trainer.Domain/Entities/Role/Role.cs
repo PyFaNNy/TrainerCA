4534a89b5ef684b49dd138df3877b03c0b8f666a
Artyom Puzyrev <fanny.koob@gmail.com>
2022-03-07 10:34:13 +0300

    Domain

---
+++ Trainer/Trainer.Domain/Entities/Role/Role.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Trainer.Domain.Entities.Role
+{
+    public class Role : IdentityRole<Guid>
+    {
+        public Role()
+        {
+
+        }
+        public Role(string roleName)
+        {
+            Name = roleName;
+            NormalizedName = roleName.ToUpper();
+        }
+    }
+}
