737f3d59a38c34d0cc8db0ab7492a180b4f0dcea
Artyom Puzyrev <fanny.koob@gmail.com>
2022-03-12 18:06:08 +0300

    add OpenIdDict

---
+++ Trainer/Trainer.OpenIdDict/DefaultData.cs
@@ -0,0 +1,67 @@
+﻿namespace Trainer.OpenIdDict
+{
+    using global::OpenIddict.Abstractions;
+    using Trainer.Persistence;
+
+    public class DefaultData : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IConfiguration _configuration;
+
+        public DefaultData(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            this._serviceProvider = serviceProvider;
+            this._configuration = configuration;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = this._serviceProvider.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<TrainerDbContext>();
+            await context.Database.EnsureCreatedAsync(cancellationToken);
+
+            var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+
+            if (await manager.FindByClientIdAsync("trainer-user", cancellationToken) is null)
+            {
+                await manager.CreateAsync(new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "trainer-user",
+                    ClientSecret = this._configuration["ClientAppSecret"],
+                    DisplayName = "TrainerUser",
+                    Permissions =
+                    {
+                        OpenIddictConstants.Permissions.Endpoints.Token,
+                        OpenIddictConstants.Permissions.Endpoints.Logout,
+                        OpenIddictConstants.Permissions.GrantTypes.Password,
+                        OpenIddictConstants.Permissions.GrantTypes.RefreshToken,
+                        OpenIddictConstants.Permissions.Prefixes.Scope + "api"
+
+                    }
+                }, cancellationToken);
+            }
+
+            if (await manager.FindByClientIdAsync("prixy-admin", cancellationToken) is null)
+            {
+                await manager.CreateAsync(new OpenIddictApplicationDescriptor
+                {
+                    ClientId = "trainer-admin",
+                    ClientSecret = this._configuration["AdminAppSecret"],
+                    DisplayName = "TrainerAdmin",
+                    Permissions =
+                    {
+                        OpenIddictConstants.Permissions.Endpoints.Token,
+                        OpenIddictConstants.Permissions.Endpoints.Logout,
+                        OpenIddictConstants.Permissions.GrantTypes.Password,
+                        OpenIddictConstants.Permissions.GrantTypes.RefreshToken,
+                        OpenIddictConstants.Permissions.Prefixes.Scope + "api"
+
+                    }
+                }, cancellationToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
---
+++ Trainer/Trainer.OpenIdDict/Startup.cs
@@ -0,0 +1,127 @@
+﻿namespace Trainer.OpenIdDict
+{
+    using global::OpenIddict.Validation.AspNetCore;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.IdentityModel.Tokens;
+    using Persistence;
+    using Settings;
+    using static global::OpenIddict.Abstractions.OpenIddictConstants;
+
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+            this.Env = env;
+        }
+
+        private IConfiguration Configuration
+        {
+            get;
+        }
+
+        private IWebHostEnvironment Env
+        {
+            get;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllersWithViews();
+
+            services.AddPersistence(this.Configuration);
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.ClaimsIdentity.UserNameClaimType = Claims.Name;
+                options.ClaimsIdentity.UserIdClaimType = Claims.Subject;
+                options.ClaimsIdentity.RoleClaimType = Claims.Role;
+                options.ClaimsIdentity.EmailClaimType = Claims.Email;
+            });
+
+            services.AddOpenIddict()
+                 .AddCore(options =>
+                 {
+                     options.UseEntityFrameworkCore()
+                         .UseDbContext<TrainerDbContext>()
+                         .ReplaceDefaultEntities<Guid>();
+                 })
+                .AddServer(options =>
+                {
+                    options.SetTokenEndpointUris("/connect/token")
+                        .SetLogoutEndpointUris("/connect/logout");
+
+                    options.AllowPasswordFlow()
+                        .AllowRefreshTokenFlow();
+
+                    if (this.Env.IsDevelopment())
+                    {
+                        options
+                            .AddDevelopmentSigningCertificate();
+                    }
+                    else
+                    {
+                        options
+                        .AddSigningCertificate(this.Configuration["SigningCertificateThumbprint"]);
+                    }
+
+
+                    options.AddEncryptionKey(new SymmetricSecurityKey(
+                        Convert.FromBase64String(this.Configuration["JWTEncryptionKey"])));
+
+                    options.SetAccessTokenLifetime(TimeSpan.FromMinutes(5));
+
+                    options.UseAspNetCore()
+                        .EnableTokenEndpointPassthrough()
+                        .EnableLogoutEndpointPassthrough();
+                })
+                .AddValidation(options =>
+                {
+                    options.UseLocalServer();
+                    options.UseAspNetCore();
+                });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+            });
+            services.AddCors();
+            services.AddHostedService<DefaultData>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors(x => x.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(options =>
+            {
+                options.MapControllers();
+                options.MapDefaultControllerRoute();
+            });
+            app.UseWelcomePage();
+        }
+    }
+}
