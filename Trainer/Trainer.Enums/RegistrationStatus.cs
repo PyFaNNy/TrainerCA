a2116fb18a04c608367ebc730d1212f7f4a0a859
Artyom Puzyrev <fanny.koob@gmail.com>
2022-03-12 18:08:08 +0300

    Enums

---
+++ Trainer/Trainer.Enums/RegistrationStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Prixy.Enums
+{
+    public enum RegistrationStatus
+    {
+        Active = 1,
+        Email,
+        PendingEmailVerification,
+        PendingKYC,
+        PendingApproval,
+        ReadyToStart,
+        Declined
+    }
+}
