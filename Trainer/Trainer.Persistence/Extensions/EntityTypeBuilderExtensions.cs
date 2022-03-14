35fd06e8e9acfa5662d947d5790b63f5b953ff05
Artyom Puzyrev <fanny.koob@gmail.com>
2022-03-14 14:18:14 +0300

    commit

--- Trainer/Trainer.Persistence/Extensions/EntityTypeBuilderExtensions.cs
+++ Trainer/Trainer.Persistence/Extensions/EntityTypeBuilderExtensions.cs
@@ -1,22 +1,23 @@
 ﻿namespace Trainer.Persistence.Extensions
-
-using Domain.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-public static class EntityTypeBuilderExtensions
 {
-    public static EntityTypeBuilder<TEntity> MapTable<TEntity>(this EntityTypeBuilder<TEntity> builder,
-        string tableName, string? schemaName = null)
-        where TEntity : class, IBaseEntity
+    using Domain.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public static class EntityTypeBuilderExtensions
     {
-        builder.ToTable(tableName, schemaName);
+        public static EntityTypeBuilder<TEntity> MapTable<TEntity>(this EntityTypeBuilder<TEntity> builder,
+            string tableName, string? schemaName = null)
+            where TEntity : class, IBaseEntity
+        {
+            builder.ToTable(tableName, schemaName);
 
-        builder.HasKey(x => x.Id);
-        builder.Property(x => x.Id)
-            .ValueGeneratedOnAdd()
-            .HasDefaultValueSql("newsequentialid()");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .HasDefaultValueSql("newsequentialid()");
 
-        return builder;
+            return builder;
+        }
     }
 }
